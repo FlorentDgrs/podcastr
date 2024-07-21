505f00f79842a7d070c37f6d15676d925b269dc8
Florent <florent.degeorges@gmail.com>
2024-07-21 21:54:22 +0200

    integration auth with clerk and db convex with middleware and webhook for users

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://mint-termite-66.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
